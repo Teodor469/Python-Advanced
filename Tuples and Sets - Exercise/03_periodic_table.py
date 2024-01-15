3e20b5e2de5058ab049cbe6aeb920f66c47c2f04
Teodor469 <lifetroll6@gmail.com>
2024-01-15 19:12:35 +0200

    Update 03_periodic_table.py

---
+++ Tuples and Sets - Exercise/03_periodic_table.py
@@ -0,0 +1,9 @@
+# table = set()
+
+# for _ in range(int(input())):
+#     for el in input().split():
+#         table.add(el)
+
+# print(*table, sep="\n")
+
+print(*{el for _ in range(int(input())) for el in input().split()}, sep="\n")
