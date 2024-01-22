40581c694c1a4c710ed56ce131b4e0dfb1593bcb
Teodor469 <lifetroll6@gmail.com>
2024-01-22 18:10:21 +0200

    .

--- Multidimensional Lists - Exercise/01_flatten_lists.py
+++ Multidimensional Lists - Exercise/01_flatten_lists.py
@@ -1,6 +1,15 @@
-matrix = [el.split() for el in input().split('|')]
+# matrix = [el.split() for el in input().split('|')]
 
-for cage in reversed(matrix):
-    if cage:
-        result = ' '.join(cage)
-        print(result, end=" ")
+# for cage in reversed(matrix):
+#     if cage:
+#         result = ' '.join(cage)
+#         print(result, end=" ")
+
+line = input().split("|")
+
+sub_list = []
+
+for sub_string in line[::-1]:
+    sub_list.extend(sub_string.split())
+
+print(*sub_list)
