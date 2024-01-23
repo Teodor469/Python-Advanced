9f031501fa33147b9f66e2d92c53bf6b3c090e1f
Teodor469 <lifetroll6@gmail.com>
2024-01-23 19:53:06 +0200

    Update 03_cheese_showcase.py

--- Functions Advanced/03_cheese_showcase.py
+++ Functions Advanced/03_cheese_showcase.py
@@ -1,2 +1,7 @@
-def sorting_cheeses():
-    pass
+def sorting_cheeses(**kwargs):
+    result = ""
+    sorted_result = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
+    for cheese_name, quantity in sorted_result:
+        result += cheese_name + "\n"
+        for q in sorted(quantity, reverse=True):
+            result += quantity + "\n"
