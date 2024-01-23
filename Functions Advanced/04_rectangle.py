03feea96fc0ba0a5c627be3ad8270bf7b001e3f9
Teodor469 <lifetroll6@gmail.com>
2024-01-23 20:36:39 +0200

    Update 04_rectangle.py

--- Functions Advanced/04_rectangle.py
+++ Functions Advanced/04_rectangle.py
@@ -9,3 +9,15 @@ def rectangle(length, width):
 
 
 print(rectangle(2, 10))
+
+def rectangle(length, width):
+    if not isinstance(length, int) or not isinstance(width, int):
+        return "Enter valid values!"
+    
+    def area():
+        return length * width
+    
+    def perimeter():
+        return (length + width) * 2
+    
+    return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
