3d197bae852c686a1739d7cb29e3f5d961584a30
Teodor469 <lifetroll6@gmail.com>
2024-01-23 13:39:14 +0200

    Create 05_operate.py

---
+++ Functions Advanced/05_operate.py
@@ -0,0 +1,17 @@
+def operate(operator, *nums):
+    result = nums[0]
+
+    for n in nums[1:]:
+        if operator == '+':
+            result += n
+        elif operator == '-':
+            result -= n
+        elif operator == '*':
+            result *= n
+        elif operator == '/':
+            if n != 0:
+                result /= n
+    return result
+
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
