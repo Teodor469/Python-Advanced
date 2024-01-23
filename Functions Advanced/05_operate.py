adb1edb4e4c1e5061057a7ef5267f6e8690e0792
Teodor469 <lifetroll6@gmail.com>
2024-01-23 20:40:56 +0200

    Update 05_operate.py

--- Functions Advanced/05_operate.py
+++ Functions Advanced/05_operate.py
@@ -29,3 +29,6 @@ def operate(operator, *args):
         return reduce(lambda x, y: x*y, args)
     elif operator == '/':
         return reduce(lambda x, y: x/y, args)
+    
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
