fd8be128467a8782aeb5867d6659b95fe1c6673a
Teodor469 <lifetroll6@gmail.com>
2024-01-25 20:49:32 +0200

    Create 07_grocery.py

---
+++ Functions Advanced -Exercise/07_grocery.py
@@ -0,0 +1,9 @@
+def grocery_store(**products):
+    products = sorted(products.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+    result = []
+
+    for product, quantity in products:
+        result.append(f"{product}: {quantity}")
+    return "\n".join(result)
+
+    # return "\n".join(f"{p}: {q}" for p, q in products)
