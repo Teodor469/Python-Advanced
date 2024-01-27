fcf816111921ec099b120f47ca29851cf96d77b2
Teodor469 <lifetroll6@gmail.com>
2024-01-27 16:58:01 +0200

    Create 01_worms_and_holes.py

---
+++ exam preperation/01_worms_and_holes.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+worms = [int(el) for el in input().split()]
+holes = deque([int(el) for el in input().split()])
+
+matches = 0
+while worms and holes:
+    worm = worms.pop()
+    hole = holes[0]
+    if worm == hole:
+        matches += 1
+        holes.popleft()
+    else:
+        worm -= 3
+        holes.popleft()
+        if worm > 0:
+            worms.append(worm)
+
+print(f"Matches: {matches}" if matches > 0 else "There are no matches.")
+print(f"Worms left: {' '.join(map(str, worms))}" if worms else "Worms left: none")
+print(f"Holes left: {' '.join(map(str, holes))}" if holes else "Holes left: none")
