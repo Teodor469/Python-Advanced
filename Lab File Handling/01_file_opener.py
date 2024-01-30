761f5ab95e0d7f98052e7c71c2dc97cc60706c96
Teodor469 <lifetroll6@gmail.com>
2024-01-30 19:10:06 +0200

    Update 01_file_opener.py

--- Lab File Handling/01_file_opener.py
+++ Lab File Handling/01_file_opener.py
@@ -2,8 +2,11 @@ import os
 
 file_name = "text.txt"
 
-try:
-    file = open(file_name)
-    print("File found")
-except FileNotFoundError:
-    print("File is not found")
+if os.path.exists(file_name):
+    try:
+        with open(file_name) as file:
+            print("File found")
+    except FileNotFoundError:
+        print("File is not found despite existence. There might be an issue with file permissions.")
+else:
+    print("File does not exist in the specified location.")
