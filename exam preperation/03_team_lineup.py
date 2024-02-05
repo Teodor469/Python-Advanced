3b1089457c6fbeae7498a65a6345421c1b8e1eee
Teodor469 <lifetroll6@gmail.com>
2024-02-05 11:28:09 +0200

    Update 03_team_lineup.py

--- exam preperation/03_team_lineup.py
+++ exam preperation/03_team_lineup.py
@@ -1,2 +1,28 @@
-def team_lineup(**kwargs):
-    sorted_result = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
+def team_lineup(*args):
+    from collections import defaultdict
+
+    players_by_country = defaultdict(list)
+    for player, country in args:
+        players_by_country[country].append(player)
+
+    sorted_countries = sorted(players_by_country.items(), key=lambda x: (-len(x[1]), x[0]))
+
+    result = []
+    for country, players in sorted_countries:
+        result.append(f"{country}:")
+        for player in players:
+            result.append(f"  -{player}")
+
+    return "\n".join(result)
+
+
+print(team_lineup(
+   ("Harry Kane", "England"),
+   ("Manuel Neuer", "Germany"),
+   ("Raheem Sterling", "England"),
+   ("Toni Kroos", "Germany"),
+   ("Cristiano Ronaldo", "Portugal"),
+   ("Thomas Muller", "Germany"),
+   ("Bruno Fernandes", "Portugal"),
+   ("Bernardo Silva", "Portugal"),
+   ("Harry Maguire", "England")))
