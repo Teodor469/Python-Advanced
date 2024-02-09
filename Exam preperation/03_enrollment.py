62867e1b7ca502d06f738d2d4214150c85654171
Teodor469 <lifetroll6@gmail.com>
2024-02-09 11:57:58 +0200

    Update 03_enrollment.py

--- Exam preperation/03_enrollment.py
+++ Exam preperation/03_enrollment.py
@@ -3,13 +3,16 @@ def gather_credits(number_of_credits_needed, *args):
     total_credits = 0
 
     for course_name, credits in args:
-        if course_name not in enrolled_courses:
-            enrolled_courses.add(course_name)
-            total_credits += credits
-
         if total_credits >= number_of_credits_needed:
-            sorted_courses = sorted(enrolled_courses)
-            return f"Enrollment finished! Maximum credits: {total_credits}.\nCourses: {', '.join(sorted_courses)}"
+            break
 
-    credits_shortage = number_of_credits_needed - total_credits
-    return f"You need to enroll in more courses! You have to gather {credits_shortage} credits more."
+        if course_name not in enrolled_courses:
+            total_credits += credits
+            enrolled_courses.add(course_name)
+
+    if total_credits >= number_of_credits_needed:
+        sorted_courses = sorted(enrolled_courses)
+        return f"Enrollment finished! Maximum credits: {total_credits}.\nCourses: {', '.join(sorted_courses)}"
+    else:
+        credits_shortage = number_of_credits_needed - total_credits
+        return f"You need to enroll in more courses! You have to gather {credits_shortage} credits more."
