7a016f3c3c116fdb0279fa61d6c55a95a519d353
Teodor469 <lifetroll6@gmail.com>
2024-02-09 20:14:40 +0200

    Update 01_worms_and_holes.py

--- exam preperation/01_worms_and_holes.py
+++ exam preperation/01_worms_and_holes.py
@@ -3,19 +3,36 @@ from collections import deque
 worms = [int(el) for el in input().split()]
 holes = deque([int(el) for el in input().split()])
 
-matches = 0
-while worms and holes:
-    worm = worms.pop()
-    hole = holes[0]
-    if worm == hole:
-        matches += 1
-        holes.popleft()
-    else:
-        worm -= 3
-        holes.popleft()
-        if worm > 0:
-            worms.append(worm)
+count = 0
+all_worms_count = len(worms)
 
-print(f"Matches: {matches}" if matches > 0 else "There are no matches.")
-print(f"Worms left: {', '.join(map(str, worms))}" if worms else "Worms left: none")
-print(f"Holes left: {', '.join(map(str, holes))}" if holes else "Holes left: none")
+while worms and holes:
+    current_worm = worms[-1]
+    current_hole = holes[0]
+
+    if current_worm == current_hole:
+        worms.pop()
+        holes.popleft()
+        count += 1
+    else:
+        holes.popleft()
+        worms[-1] -= 3
+        if worms[-1] <= 0:
+            worms.pop()
+
+if count:
+    print(f"Matches: {count}")
+else:
+    print("There are no matches.")
+
+if not worms and count == all_worms_count:
+    print("Every worm found a suitable hole!")
+elif not worms and count < all_worms_count:
+    print("Worms left: none")
+else:
+    print(f"Worms left: {', '.join([str(el) for el in worms])}")
+
+if not holes:
+    print("Holes left: none")
+else:
+    print(f"Holes left: {', '.join([str(el) for el in holes])}")
