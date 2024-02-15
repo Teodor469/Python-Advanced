c0432c1a88a6476f9497606da49d2c5c33fb75d9
Teodor469 <lifetroll6@gmail.com>
2024-02-15 11:21:29 +0200

    Update 01_rubber_duck_debuggers.py

--- Exam preperation/01_rubber_duck_debuggers.py
+++ Exam preperation/01_rubber_duck_debuggers.py
@@ -21,23 +21,19 @@ def calculate_ducky(time_for_completion, number_of_tasks):
         elif sequence_calc <= 240:
             ducky_type = "Small Yellow Rubber Ducky"
         else:
-            # If the calculated time goes above the highest range
-            number_of_tasks[-1] -= 2  # decrease the number of the tasks' value by 2
-            time_for_completion.append(time_for_completion.popleft())  # move the programmer time's value to the end of its sequence
+            number_of_tasks[-1] -= 2
+            time_for_completion.append(time_for_completion.popleft())
             continue
 
         ducky_counts[ducky_type] += 1
-        # Remove the used values
         time_for_completion.popleft()
         number_of_tasks.pop()
 
-    # Output the type and number of ducks given
     for ducky, count in ducky_counts.items():
         print(f"{ducky}: {count}")
 
     return ducky_counts
 
-# Test the function
 time_for_completion = deque([int(el) for el in input().split()])
 number_of_tasks = [int(el) for el in input().split()]
 print("Congratulations, all tasks have been completed! Rubber ducks rewarded:")
